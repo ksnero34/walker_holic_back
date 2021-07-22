b7a1b8fb2eb39ee3df60aca3eeb93d98f4e68998
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-22 22:46:41 +0900

    commit

--- WHFlask/app.py
+++ WHFlask/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, url_for, request, render_template, jsonify
+from flask import Flask, jsonify, request
 import ssl
 import requests
 # import sql_connect
@@ -11,11 +11,13 @@ application.config['JSON_AS_ASCII'] = False
 
 @application.route("/walk_data", methods = ['GET'])
 def admin_to_login2():
-    return "요청이 들어오면 db에서 공지사항 내용을 json 형식으로 웹에 보여줍니다."
+    data = {'name' : '한준규', 'test':'test입니다.'}
+    return jsonify(data)
 
-@application.route("/input", methods = ['GET'])
+@application.route("/input", methods = ['GET','POST'])
 def admin_to_login():
-    return "산책데이터를 json으로 받아 db에 넣습니다."
+    parm = reqeust.get_json()
+    return "json_data.를 받아왔습니다.",parm
 
 @application.route("/민원내용", methods = ['GET'])
 def admin_to_login3():
