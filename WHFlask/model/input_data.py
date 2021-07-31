8fea5af6ef256bcf7b4007cfcb31f59da9b2af5a
doongu <must1080@naver.com>
2021-08-01 01:56:53 +0900

    c

--- WHFlask/model/input_data.py
+++ WHFlask/model/input_data.py
@@ -13,7 +13,7 @@ def input_report_data(json_data):
     date = str(json_data['date'])
     test = "test_title"
     # 잘 되는뎅 json_data 문제, format 문제 
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test','test','test','test','test','test')""")
+    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test','test','test','test','test',{test})""")
     conn.commit()
     # title
     query ="""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', %s, %s, %s, %s, %s, %s)"""
