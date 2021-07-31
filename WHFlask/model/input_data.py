4c1ed7c932448577c0ae4dfd6a76cd8d54e1ac27
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-31 20:43:11 +0900

    test python to mysql

--- WHFlask/model/input_data.py
+++ WHFlask/model/input_data.py
@@ -5,6 +5,8 @@ curs = conn.cursor()
 
 def input_report_data(json_data):
     global curs,conn
+    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test','test','test','test')""")
+    conn.commit()
 
     title = json_data['title']
     content = json_data['content']
@@ -13,9 +15,9 @@ def input_report_data(json_data):
     longitude = json_data['longitude']
     data = json_data['date'] 
 
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', {title}, {content}, {image}, {latitude},{longitude},{data} )""")
-    print(" 데이터를 추가하였습니다 ! ")
-    conn.commit()
+    # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', {title}, {content}, {image}, {latitude},{longitude},{data} )""")
+    # print(" 데이터를 추가하였습니다 ! ")
+    # conn.commit()
 
 
 def input_walk_data(json_data):
