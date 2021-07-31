743c71f7b0a6d175e451ddbce08b84a0de5ca603
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-31 21:28:13 +0900

    tester

--- WHFlask/application.py
+++ WHFlask/application.py
@@ -5,7 +5,10 @@ import ssl
 import requests
 from controller import json_handler
 # import sql_connect
+import sys
 
+print('This is error output', file=sys.stderr)
+print('This is standard output', file=sys.stdout)
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False #
 
@@ -19,10 +22,11 @@ def admin_to_login2():
 
 @app.route("/input", methods = ['GET','POST'])
 def admin_to_login():
-    print("123123123")
+    print("123123123",  file=sys.stderr)
     parm = request.get_json()
     json_handler.input_json_data(parm)
-    print(parm['type'])
+    print(parm['type'],  file=sys.stderr)
+    print(parm, file=sys.stderr)
     return parm['type']
 
 
