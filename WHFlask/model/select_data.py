265392a71ebe47d6f4ac03bc44720e907755c636
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-08-12 20:03:33 +0900

    commit

--- WHFlask/model/select_data.py
+++ WHFlask/model/select_data.py
@@ -1,15 +1,19 @@
 import pymysql
 '''connect doongu, compTBL'''
-conn = pymysql.connect(host='localhost', user='doongu', password ='wnsrb12', db='walker_holic',port =3306, charset='utf8')
-curs = conn.cursor()
 
 def select_noticeTBL():
-    global curs,conn
+    conn = pymysql.connect(host='localhost', user='doongu', password ='wnsrb12', db='walker_holic',port =3306, charset='utf8')
     
-    query ="""select * from noticeTBL"""
-    curs.execute(query)
-    render_select_data = curs.fetchall()
-    conn.close()
+    try:
+        with conn.cursor() as curs:
+        
+            query ="""select * from noticeTBL"""
+            curs.execute(query)
+            
+            render_select_data = curs.fetchall()
+    finally:
+        conn.close()
+
     return render_select_data
 
     
