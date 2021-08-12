91f36aff6800a7035f7e083c4a0573acf335e076
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-08-12 17:14:38 +0900

    test

--- WHFlask/application.py
+++ WHFlask/application.py
@@ -8,6 +8,8 @@ from controller import json_handler
 import sys
 from werkzeug.utils import secure_filename
 
+from model import input_data
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False #
 
@@ -18,21 +20,39 @@ def admin_to_login2():
 	     {'id' : 2, 'title': '이선우', 'url' : 'ㅋㅋ'}]
     return jsonify(data)
 
-@app.route("/input", methods = ['GET','POST'])
-def admin_to_login():
+@app.route("/input_report_data", methods = ['POST'])
+def report_data_to_db():
     type = request.form["type"]
     title = request.form["title"]
-    file_img = request.files['image']
-    file_img.save(secure_filename(file_img.filename))
+    content = request.form["content"]
+    image = request.files['image']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    date = request.form['date']
+    image.save(secure_filename(image.filename))
 
+    input_data.input_report_data(type, title, content, image.filename, latitude, longitude, date)
 
 
     return "hello"
 
+
+@app.route("/input_walk_log", methods = ['POST'])
+def walk_data_to_db():
+    json_data = request.get_json()
+    destination = json_data['destination']
+    diff = json_data['diff']
+    start = json_data['start']
+    end = json_data['end']
+
+    input_data.input_walk_data(destination, diff, start, end)
+
+    return "hello"
 
 @app.route("/", methods = ["GET"])
 def hello():
     return "hello"
+
 # 테스트를위한 주석입니다. 
 if __name__ == '__main__':
     app.run( port = 5000)
--- WHFlask/model/input_data.py
+++ WHFlask/model/input_data.py
@@ -3,15 +3,9 @@ import pymysql
 conn = pymysql.connect(host='localhost', user='doongu', password ='wnsrb12', db='walker_holic',port =3306, charset='utf8')
 curs = conn.cursor()
 
-def input_report_data(json_data):
+def input_report_data(type, title, content, image, latitude, longitude, date):
     global curs,conn
-    title = str(json_data['title'])
-    content = str(json_data['content'])
-    image = str(json_data['image'])
-    latitude = str(json_data['latitude'])
-    longitude = str(json_data['longitude'])
-    date = str(json_data['date'])
-    test = "test_title"
+    
     # ??? f를 이용해서 쿼리 날리면 안된다. ㅋㅋ
     # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test','test','test','test','test','test','test')""")
     # conn.commit()
@@ -20,43 +14,41 @@ def input_report_data(json_data):
     # curs.execute(query, ('fuckyou'))
     # conn.commit()
     # title
-    query ="""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', %s, %s, "fuckimage", "fuckimage","fuckimage", "fuck_date")"""
-    curs.executemany(query, ( title, content ))
-    conn.commit()
-    # content
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', {content},'test','test','test','test')""")
+    query ="""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), %s, %s, %s, %s, %s,%s, %s)"""
+    curs.executemany(query, (type, title, content, image, latitude, longitude, date));
     conn.commit()
 
-    # image
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test', {image},'test','test','test')""")
-    conn.commit()
+    '''
+    test
+    '''
+    # # content
+    # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', {content},'test','test','test','test')""")
+    # conn.commit()
 
-    # latitude
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test',{latitude},'test','test')""")
-    conn.commit()
+    # # image
+    # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test', {image},'test','test','test')""")
+    # conn.commit()
 
-    # longitude
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test','test',{longitude},'test')""")
-    conn.commit()
+    # # latitude
+    # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test',{latitude},'test','test')""")
+    # conn.commit()
 
-    # longitude
-    curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test','test','test',{date})""")
-    conn.commit()
+    # # longitude
+    # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test','test',{longitude},'test')""")
+    # conn.commit()
+
+    # # longitude
+    # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', 'test', 'test','test','test','test',{date})""")
+    # conn.commit()
 
     # curs.execute(f"""Insert into compTBL VALUES ( (SELECT IFNULL(MAX(compID) + 1, 1) FROM compTBL test), 'test', {title}, {content}, {image}, {latitude},{longitude},{data} )""")
     # print(" 데이터를 추가하였습니다 ! ")
     # conn.commit()
 
 
-def input_walk_data(json_data):
+def input_walk_data(destination, diff, start, end):
     global curs, conn
-
-    destination = json_data['destination']
-    diff = json_data['diff']
-    start = json_data['start']
-    end = json_data['end']
     
-    curs.execute(f"""Insert into walkTBL VALUES ( (SELECT IFNULL(MAX(datacode) + 1, 1) FROM walkTBL tester), 'test', {destination}, {diff}, {start}, {end} )""")
-
-    print(" 데이터를 추가하였습니다 ! ")
+    query = """Insert into walkTBL VALUES ( (SELECT IFNULL(MAX(datacode) + 1, 1) FROM walkTBL tester), 'test', %s, %s, %s, %s)"""
+    curs.executemany(query, (destination, diff, start, end))
     conn.commit()
