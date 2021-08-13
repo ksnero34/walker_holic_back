525707cbf830f83855e10720e4eaf33d3ff0d338
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-08-13 21:35:18 +0900

    test

---
+++ WHFlask/config.py
@@ -0,0 +1,10 @@
+import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+class config:
+
+    SLOW_API_TIME = 0.5
+    print(1)
+    @staticmethod
+    def init_app(app):
+        pass
---
+++ WHFlask/model/__init__.py
@@ -0,0 +1,19 @@
+'''
+Application Model
+'''
+
+# from model import mysql
+from config import config
+
+def get_cursor():
+    print(config.SLOW_API_TIME)
+
+
+def init_app():
+    '''Model Init Function'''
+
+    # Mysql Init
+    initializer = mysql.ModelInitializer()
+    initializer.init_model()
+
+get_cursor()
